4aa768e8256c0060616e3d978c103ddf2c181019
Resad Memmedov <resad.memmedov@hotmail.com>
2023-11-08 11:12:41 +0100

    chat message fix

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -66,7 +66,7 @@ export const ChatMessages = ({
     count: data?.pages?.[0]?.items?.length ?? 0,
   });
 
-  if (status === "loading") {
+  if (status.toString() === "loading") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
         <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
@@ -77,7 +77,7 @@ export const ChatMessages = ({
     );
   }
 
-  if (status === "error") {
+  if (status.toString() === "error") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
         <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
